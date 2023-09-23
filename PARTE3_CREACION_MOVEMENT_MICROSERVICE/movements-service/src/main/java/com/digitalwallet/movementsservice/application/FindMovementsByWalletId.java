5c81e2e014a1966b309a250f7c86f2eeae7800bb
Luis Antonio Olivera Vasquez <lolivera@bancomercio.com.pe>
2023-09-23 13:31:19 -0500

    Correccion del microservicio

--- PARTE3_CREACION_MOVEMENT_MICROSERVICE/movements-service/src/main/java/com/digitalwallet/movementsservice/application/FindMovementsByWalletId.java
+++ PARTE3_CREACION_MOVEMENT_MICROSERVICE/movements-service/src/main/java/com/digitalwallet/movementsservice/application/FindMovementsByWalletId.java
@@ -2,15 +2,22 @@ package com.digitalwallet.movementsservice.application;
 
 import com.digitalwallet.movementsservice.domain.Movement;
 import com.digitalwallet.movementsservice.domain.MovementRepository;
+import com.digitalwallet.movementsservice.domain.exceptions.DigitalWalletGenericClientException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class FindMovementsByWalletId {
     private final MovementRepository movementRepository;
-    public List<Movement> findByWalletId(String walletId){
-        return movementRepository.findMovementsByWalletId(walletId);
+
+    public List<Movement> findByWalletId(String walletId) {
+        List<Movement> movementList = movementRepository.findMovementsByWalletId(walletId);
+        if (movementList.isEmpty())
+            throw new DigitalWalletGenericClientException("List is empty", HttpStatus.NO_CONTENT);
+        return movementList;
     }
 }
