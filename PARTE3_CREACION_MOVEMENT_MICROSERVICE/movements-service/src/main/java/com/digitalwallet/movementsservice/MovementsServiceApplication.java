f813f58f1490caf6c466713bd70e78348e7480b3
Luis Antonio Olivera Vasquez <lolivera@bancomercio.com.pe>
2023-09-23 10:53:33 -0500

    Proyecto inicializado

---
+++ PARTE3_CREACION_MOVEMENT_MICROSERVICE/movements-service/src/main/java/com/digitalwallet/movementsservice/MovementsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.digitalwallet.movementsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovementsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovementsServiceApplication.class, args);
+	}
+
+}
---
+++ PARTE3_CREACION_MOVEMENT_MICROSERVICE/movements-service/src/test/java/com/digitalwallet/movementsservice/MovementsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalwallet.movementsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovementsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
